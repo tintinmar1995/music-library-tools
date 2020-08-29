79b87d7c6e239dfc128488cec818247c164490da
tintinmar1995 <martin.masson@protonmail.com>
2020-08-29 12:28:47 +0200

    Docker config with semantic plugin. Install pypher.

--- neo4j/quick-start.sh
+++ neo4j/quick-start.sh
@@ -7,15 +7,16 @@
 docker pull neo4j
 
 docker run \
+     --name=neo4j-rdf \
      --publish=7474:7474 --publish=7687:7687 \
      --volume=$HOME/neo4j/data:/data \
-     --env NEO4J_AUTH=neo4j/neo \
-     neo4j
+     --env='NEO4J_AUTH=neo4j/neo' \
+     --env='NEO4JLABS_PLUGINS=["n10s"]' \
+     neo4j:4.0.7
 
 
 ## Python
 # Driver 
 pip3 install neo4j
-# Library
-pip3 install py2neo
-
+# Libraries
+pip3 install py2neo pypher
